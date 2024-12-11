cc8a78d0b3f581040ea4e36d2847cef0f588254b
Marcin Izdebski <mizdebski123@gmail.com>
2024-12-11 12:36:55 +0100

    Texts added

--- src/features/Hero/hero.tsx
+++ src/features/Hero/hero.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { TextSection, Title, Wrapper } from "./styledHero";
+import { Role, Text, TextSection, Title, Wrapper } from "./styledHero";
 import { TypeAnimation } from "react-type-animation";
 
 export const Hero = () => {
@@ -14,6 +14,15 @@ export const Hero = () => {
                         repeat={Infinity}
                     />
                 </Title>
+                <Role>Frontend / Mobile App developer</Role>
+                <Text>
+                    I'm an enthusiastic Frontend Developer with a strong passion
+                    for new technologies and a deep commitment to learning
+                    React. I am highly motivated and thrive on being a fast
+                    learner. Currently, I am actively seeking new job
+                    opportunities to further enhance my skills and contribute to
+                    innovative projects.
+                </Text>
             </TextSection>
         </Wrapper>
     );
--- src/features/Hero/styledHero.ts
+++ src/features/Hero/styledHero.ts
@@ -3,10 +3,18 @@ import { theme } from '../../core/theme';
 
 export const Wrapper = styled.div`
     min-height: 100vh;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    align-items: center;
+    justify-content: center;
+    padding: 40px;
 `;
 
 export const TextSection = styled.div`
-
+    display: grid;
+    align-items: center;
+    justify-content: center;
+    gap: 32px;
 `;
 
 export const Title = styled.h1`
@@ -14,10 +22,30 @@ export const Title = styled.h1`
     font-weight: normal;
     color: ${theme.color.fontColor};
     font-weight: 100;
+    text-align: center;
+    width: 600px;
+    margin: 0;
+`;
+
+export const Role = styled.h2`
+    font-size: 32px;
+    font-weight: 300;
+    background: linear-gradient(to right, #195a58 0%, #ffffff 100%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    margin: 0;
 `;
 
 export const Text = styled.p`
+    font-size: 24px;
+    color: ${theme.color.fontColor};
+    font-weight: 200;
+    text-align: justify;
+    margin: 0;
+`;
 
+export const Image = styled.img`
+    
 `;
 
 export const Button = styled.button`
