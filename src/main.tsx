add0ebb85d73c3d60963e00fb3c7a6f2bfa9301d
Marcin Izdebski <mizdebski123@gmail.com>
2024-12-11 12:00:52 +0100

    styled components added

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,12 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./core/App.tsx";
+import { GlobalStyles } from "./core/GlobalStyles.ts";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./core/theme.ts";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+    <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <App />
+    </ThemeProvider>
+);
